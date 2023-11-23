10986bda6d8d4153c77a9c37573162fb78325d43
NickLarsenNZ <nick@aptiv.co.nz>
2023-11-23 23:15:31 +0100

    feat: serialize/deserialize the Config struct
    ```yaml
    max_parallel: 2
    reqs:
    - GET: /
      body_json:
        client_id: abc
        client_secret: xyz
      print: ''
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::collections::HashMap;
+
+use crate::config::{Action, Config, Req};
+
+mod config;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config {
+        reqs: vec![Req {
+            // id: todo!(),
+            // method: todo!(),
+            body: Some(config::Body::BodyJson(HashMap::from([
+                ("client_id".to_owned(), "abc".to_owned()),
+                ("client_secret".to_owned(), "xyz".to_owned()),
+            ]))),
+            action: Some(Action::Print("".to_owned())),
+            ..Default::default()
+        }],
+        ..Default::default()
+    };
+
+    let yaml = serde_yaml::to_string(&config).expect("to be able to serialize");
+
+    println!("{yaml}");
 }
